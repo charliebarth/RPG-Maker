c0bd348a5b662f610cc77c3277211f32d461632a
Charlie Barth <charliepbarth@gmail.com>
2021-06-27 00:35:36 -0400

    Added Create, Index, Update, and Destroy actions

--- backend/app/controllers/perks_controller.rb
+++ backend/app/controllers/perks_controller.rb
@@ -1,2 +1,54 @@
 class PerksController < ApplicationController
+    def create
+        perk = Perk.create(perk_params)
+        if perk.valid?
+          render json: { perk: PerkSerializer.new(perk).serializable_hash }, status: :created
+        else 
+          render json: { error: 'failed to create perk', messages: perk.errors.full_messages }, status: :not_acceptable
+        end
+    end
+
+    def index
+        perks = Perk.all
+        if perks == nil
+            render json: { error: 'No perks created yet' }, status: :no_perks
+        else
+            render json: { perks: PerkSerializer.new(perks).serializable_hash }, status: :retrieved_perks
+        end
+    end
+
+    def update
+        if Perk.find(params[:id])
+            perk = Perk.find(params[:id])
+
+            perk_params.each do |key, value|
+                if value
+                    perk[key.to_sym] = value
+                end
+                
+            end
+
+            perk.save
+        else
+            render json: { error: "That perk doesn't appear to exist." }, status: :not_a_perk
+        end
+
+    end
+
+    def destroy
+        if current_user.admin? && Perk.find(params[:id])
+            perk = Perk.find(params[:id])
+            perk.delete
+        elsif !current_user.admin?
+            render json: { error: 'You do not have permission to delete this perk. You need to be an admin to delete perks.' }, status: :unpermitted
+        elsif !Perk.find(params[:id])
+            render json: { error: "That perk doesn't appear to exist." }, status: :not_a_perk
+        end
+    end
+
+    private
+  
+    def perk_params
+      params.require(:perk).permit(:name, :description, :modifiers)
+    end
 end

935cdbef4a2df1e9c9b66be3c708ad12801671f4
Charlie Barth <charliepbarth@gmail.com>
2021-06-29 19:47:26 -0400

    Fixed extra spaces issue

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,47 +1,47 @@
 class UsersController < ApplicationController
-    def create
-        user = User.create(user_params)
+  def create
+    user = User.create(user_params)
 
-        if user.valid?
-          render json: { user: UserSerializer.new(user).serializable_hash }, status: :created
-        else 
-          render json: { error: 'failed to create user', messages: user.errors.full_messages }, status: :not_acceptable
-        end
+    if user.valid?
+      render json: { user: UserSerializer.new(user).serializable_hash }, status: :created
+    else
+      render json: { error: 'failed to create user', messages: user.errors.full_messages }, status: :not_acceptable
     end
+  end
 
-    def index
-        users = User.all
+  def index
+    users = User.all
 
-        if users == nil
-            render json: { error: 'No users created yet' }, status: :no_users
-        else
-            render json: { users: UserSerializer.new(users).serializable_hash }, status: :retrieved_users
-        end
+    if users == nil
+      render json: { error: 'No users created yet' }, status: :no_users
+    else
+      render json: { users: UserSerializer.new(users).serializable_hash }, status: :retrieved_users
     end
+  end
 
-    def login
-      user = User.where(code: user_params[:code])
+  def login
+    user = User.where(code: user_params[:code])
 
-      if user.present? && user.name == user_params[:name]
-        found_user = User.find(user.id)
-        current_user(found_user)
-        render json: { success: 'Successfully logged in' }, status: :logged_in
-      elsif !user.present?
-        render json: { error: 'No user eixsts with this code' }, status: :not_a_user
-      elsif user.present? && user.name == !user_params[:name]
-        render json: { error: "The code you entered doens't match with the name you entered" }, status: :credentials_do_not_match
-      end
+    if user.present? && user.name == user_params[:name]
+      found_user = User.find(user.id)
+      current_user(found_user)
+      render json: { success: 'Successfully logged in' }, status: :logged_in
+    elsif !user.present?
+      render json: { error: 'No user eixsts with this code' }, status: :not_a_user
+    elsif user.present? && user.name == !user_params[:name]
+      render json: { error: "The code you entered doens't match with the name you entered" }, status: :credentials_do_not_match
     end
+  end
 
-    def logout
-      current_user(nil)
-      render json: { success: 'Successfully logged out' }, status: :logged_out
-    end
+  def logout
+    current_user(nil)
+    render json: { success: 'Successfully logged out' }, status: :logged_out
+  end
     
 
-    private
+  private
   
-    def user_params
-      params.require(:user).permit(:name, :code)
-    end
+  def user_params
+    params.require(:user).permit(:name, :code)
+  end
 end

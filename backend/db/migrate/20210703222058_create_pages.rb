dda92d5c773b48d588bf1b9d9b1218827823f45e
Charlie Barth <charliepbarth@gmail.com>
2021-07-03 18:27:14 -0400

    Updated migrations

--- backend/db/migrate/20210703222058_create_pages.rb
+++ backend/db/migrate/20210703222058_create_pages.rb
@@ -1,6 +1,9 @@
 class CreatePages < ActiveRecord::Migration[6.1]
   def change
     create_table :pages do |t|
+      t.string :title
+      t.string :description
+      t.integer :rulebook_id
 
       t.timestamps
     end
--- backend/db/migrate/20210703222203_create_rulebooks.rb
+++ backend/db/migrate/20210703222203_create_rulebooks.rb
@@ -1,6 +1,8 @@
 class CreateRulebooks < ActiveRecord::Migration[6.1]
   def change
     create_table :rulebooks do |t|
+      t.string :title
+      t.string :description
 
       t.timestamps
     end
